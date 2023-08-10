08d148a78d759f1acfd243d7730e566e47ffff0d
TogrulEminov <togrul40@mail.ru>
2023-08-10 14:50:08 +0400

    change

--- assets/javascript/loading.js
+++ assets/javascript/loading.js
@@ -1,7 +1,9 @@
 // !loading
 const loader = document.querySelector('#preloader');
+const body=document.querySelector('body')
 window.addEventListener('load', () => {
  setTimeout(() => {
   loader.style.display = 'none';
+  boy.style.overflowY= 'none';
  }, 2000);
 });
