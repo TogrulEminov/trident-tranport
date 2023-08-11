c1ca6fa803aab049fda912979b414b0ff97b6071
TogrulEminov <togrul40@mail.ru>
2023-08-11 15:54:27 +0400

    add last changes

--- assets/javascript/loading.js
+++ assets/javascript/loading.js
@@ -3,11 +3,5 @@ const loader = document.querySelector('#preloader');
 window.addEventListener('load', () => {
  setTimeout(() => {
   loader.style.display = 'none';
-<<<<<<< HEAD
- }, 2000);
-});
-
-=======
- }, 0);
-});
->>>>>>> 7ff505fc88eb56fc82a4125cd49598868f747324
+ }, 2000)
+})
