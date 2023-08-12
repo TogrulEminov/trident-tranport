52101d69db8bc2b53a285a4e6a70c9f509ee8c93
TogrulEminov <togrul40@mail.ru>
2023-08-12 23:58:53 +0400

    add language

---
+++ assets/javascript/language.js
@@ -0,0 +1,28 @@
+var demoJson = {
+  demo: {
+    home: {
+      az: 'Əsas',
+      en: 'home',
+      ru: 'Дом',
+      tr: 'ana sayfa',
+    },
+    aboutUs: {
+      az: 'Haqqımızda',
+      en: 'About',
+      ru: 'О нас',
+      tr: 'Hakkımızda',
+    },
+    calculate: {
+      az: 'Kalkulyator',
+      en: 'Calculator',
+      ru: 'Калькулятор',
+      tr: 'Hesap makinesi',
+    },
+    contact: {
+      az: 'Əlaqə',
+      en: 'Contact',
+      ru: 'Контакт',
+      tr: 'bize Ulaşın',
+    },
+  },
+};
