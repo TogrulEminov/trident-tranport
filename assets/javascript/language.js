1a87d90a53b37dfc8ded5ad058777f47e0c0d3e8
TogrulEminov <togrul40@mail.ru>
2023-08-14 15:50:01 +0400

    add changes

--- assets/javascript/language.js
+++ assets/javascript/language.js
@@ -24,5 +24,35 @@ var demoJson = {
       ru: 'Контакт',
       tr: 'bize Ulaşın',
     },
+    TransportPriceCalculator: {
+      az: 'Nəqliyyat Qiymət Kalkulyatoru',
+      en: 'Transport Price Calculator',
+      ru: 'Калькулятор стоимости транспорта',
+      tr: 'Nakliye Fiyat Hesaplayıcı',
+    },
+    calc:{
+      az: 'Hesabla',
+      en: 'Calculate',
+      ru: 'Рассчитай',
+      tr: 'Hesaplamak',
+    },
+    getQusition:{
+      az: 'Sualınız var?',
+      en: 'Have a question?',
+      ru: 'Есть вопрос?',
+      tr: 'Bir sorunuz var mı?',
+    },
+    contactUs:{
+      az: 'Bizimlə əlaqə',
+      en: 'Contact us',
+      ru: 'Связаться с нами',
+      tr: 'Bize Ulaşın',
+    },
+    writeWhatsaap:{
+      az: 'Bir kliklə whatsapp nömrəmizə yazın',
+      en: 'Write to our whatsapp number with one click',
+      ru: 'Напишите на наш номер WhatsApp в один клик',
+      tr: 'Tek tıkla whatsapp numaramıza yazın?',
+    }
   },
 };
