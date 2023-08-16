acc758df9aaebf17d5c615c9247f0be5050399a5
TogrulEminov <togrul40@mail.ru>
2023-08-16 12:04:27 +0400

    add min js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,81 +1,62 @@
-const closeIcon = document.querySelector('#close-icon');
-let hamburger = document.querySelector('.hamburger button');
-let sidebar = document.querySelector('.sidebar-nav');
-let overlay = document.querySelector('.overlay-sidebar');
-const body = document.querySelector('body');
-// !header
-window.addEventListener('scroll', function () {
-  const header = document.querySelector('.header');
-  header.classList.toggle('sticky', window.scrollY > 80);
-});
-
-// ! hamburger icon
-function hamburgerMenu(button) {
-  button.classList.toggle('change');
+const closeIcon = document.querySelector("#close-icon");
+let hamburger = document.querySelector(".hamburger button"),
+  sidebar = document.querySelector(".sidebar-nav"),
+  overlay = document.querySelector(".overlay-sidebar");
+const body = document.querySelector("body");
+function hamburgerMenu(e) {
+  e.classList.toggle("change");
 }
-
 function toggleSidebar() {
-  sidebar.classList.toggle('active');
-  hamburger.classList.remove('change');
-  sidebar.classList.contains('active')
-    ? (body.style.overflow = 'hidden')
-    : (body.style.overflow = 'visible');
-  sidebar.classList.contains('active')
-    ? hamburger.classList.add('change')
-    : hamburger.classList.remove('change');
-  overlay.style.display = sidebar.classList.contains('active')
-    ? 'block'
-    : 'none';
+  sidebar.classList.toggle("active"),
+    hamburger.classList.remove("change"),
+    sidebar.classList.contains("active")
+      ? (body.style.overflow = "hidden")
+      : (body.style.overflow = "visible"),
+    sidebar.classList.contains("active")
+      ? hamburger.classList.add("change")
+      : hamburger.classList.remove("change"),
+    (overlay.style.display = sidebar.classList.contains("active")
+      ? "block"
+      : "none");
 }
-hamburger.addEventListener('click', toggleSidebar);
-overlay.addEventListener('click', toggleSidebar);
-closeIcon.addEventListener('click', toggleSidebar);
-
-$(document).ready(function () {
-  $('.mobile-menu-item').click(function () {
-    $('.submenu-mobile').slideToggle();
-    $(this).toggleClass('open');
-  });
-});
-
-$(document).ready(function () {
-  $('#hero-slider').owlCarousel({
-    items: 1,
-    loop: true,
-    nav: false,
-    dots: true,
-    autoplay: true,
-    autoplayTimeout: 3500,
-  });
-});
-//!blog courusel
-$('.delivery-card-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  items: 5,
-  responsiveClass: true,
-  dots: false,
-  autoplay: true,
-  autoplayTimeout: 2000,
-  autoplayHoverPause: true,
-  responsive: {
-    0: {
+window.addEventListener("scroll", function () {
+  let e = document.querySelector(".header");
+  e.classList.toggle("sticky", window.scrollY > 80);
+}),
+  hamburger.addEventListener("click", toggleSidebar),
+  overlay.addEventListener("click", toggleSidebar),
+  closeIcon.addEventListener("click", toggleSidebar),
+  $(document).ready(function () {
+    $(".mobile-menu-item").click(function () {
+      $(".submenu-mobile").slideToggle(), $(this).toggleClass("open");
+    });
+  }),
+  $(document).ready(function () {
+    $("#hero-slider").owlCarousel({
       items: 1,
-      nav: false,
+      loop: !0,
+      nav: !1,
+      dots: !0,
+      autoplay: !0,
+      autoplayTimeout: 3500,
+    });
+  }), //!blog courusel
+  $(".delivery-card-carousel").owlCarousel({
+    loop: !0,
+    margin: 10,
+    nav: !0,
+    items: 5,
+    responsiveClass: !0,
+    dots: !1,
+    autoplay: !0,
+    autoplayTimeout: 2e3,
+    autoplayHoverPause: !0,
+    responsive: {
+      0: { items: 1, nav: !1 },
+      600: { items: 1 },
+      768: { items: 2 },
+      1e3: { items: 2 },
+      1200: { items: 3 },
     },
-    600: {
-      items: 1,
-    },
-    768: {
-      items: 2,
-    },
-    1000: {
-      items: 2,
-    },
-    1200: {
-      items: 3,
-    },
-  },
-});
-AOS.init();
+  }),
+  AOS.init();
--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,25 +1,30 @@
-    
 ymaps.ready(init);
-
 function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [40.372974, 49.853148],
-        zoom: 13,
-        controls: []
-    }, {
-        suppressMapOpenBlock: true
-    });
-    myMap.behaviors.disable('scrollZoom');
-    myMap.controls.add('zoomControl', {
-        position: {
-            top: 100,
-            left: 10
+  var o = new ymaps.Map(
+    "map",
+    { center: [40.372974, 49.853148], zoom: 13, controls: [] },
+    { suppressMapOpenBlock: !0 }
+  );
+  o.behaviors.disable("scrollZoom"),
+    o.controls.add("zoomControl", { position: { top: 100, left: 10 } }),
+    o.geoObjects.add(
+      new ymaps.Placemark(
+        [40.372974, 49.853148],
+        {},
+        {
+          iconLayout: "default#image",
+          iconImageHref: "./assets/media/marker.png",
+          iconImageSize: [61, 77],
+          iconImageOffset: [-42, -65],
         }
-    });
-    myMap.geoObjects.add(new ymaps.Placemark([40.372974, 49.853148], {}, {
-        iconLayout: 'default#image',
-        iconImageHref: './assets/media/marker.png',
-        iconImageSize: [61, 77],
-        iconImageOffset: [-42, -65]
-    }));
+      )
+    ),
+    mapmyMap.controls.remove("geolocationControl"),
+    mapmyMap.controls.remove("searchControl"),
+    mapmyMap.controls.remove("trafficControl"),
+    mapmyMap.controls.remove("typeSelector"),
+    mapmyMap.controls.remove("fullscreenControl"),
+    mapmyMap.behaviors.disable("scrollZoom"),
+    mapmyMap.controls.remove("rulerControl"),
+    mapmyMap.geoObjects.add(placemark);
 }
